5db43f74043595cd28525008d812f00af2c84f4e
justinkwanchan <justinkwanchan@gmail.com>
2020-12-16 19:44:16 +0000

    Dynamically add multiple tweets to DOM

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,59 +1,84 @@
 /*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+* Client-side JS logic goes here
+* jQuery is already loaded
+* Reminder: Use (and do all your DOM work in) jQuery's document ready function
+*/
 
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
-const createTweetElement = function (tweet) {
-  const $tweet = $(`
-    <article class="hvr-box-shadow-outset">
-      <header>
-        <div>
-          <div>
-            <img src="${tweet.user.avatars}">
-          </div>
-          <span>Newton</span>
-        </div>
-        
-        <span class="handle">${tweet.user.handle}</span>
-      </header>
-
-      <p>
-        ${tweet.content.text}
-      </p>
-
-      <footer>
-        <span class="tweet-age">${tweet.created_at} days ago</span>
-        <div class="icons">
-          <div class="flag"></div>
-          <div class="share"></div>
-          <div class="like"></div>
-        </div>
-      </footer>
-    </article>
-  `);
-  return $tweet;
-};
-
 $(document).ready(function() {
-  $(document).on('dblclick', function (event) {
-    console.log(createTweetElement(tweetData)[0]); // to see what it looks like
-    $('#tweets-container').append(createTweetElement(tweetData)[0]);
-  });
-});
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  const createTweetElement = function (tweet) {
+    const $tweet = $(`
+      <article class="hvr-box-shadow-outset">
+        <header>
+          <div>
+            <div>
+              <img src="${tweet.user.avatars}">
+            </div>
+            <span>${tweet.user.name}</span>
+          </div>
+          
+          <span class="handle">${tweet.user.handle}</span>
+        </header>
+
+        <p>
+          ${tweet.content.text}
+        </p>
+
+        <footer>
+          <span class="tweet-age">${tweet.created_at} days ago</span>
+          <div class="icons">
+            <div class="flag"></div>
+            <div class="share"></div>
+            <div class="like"></div>
+          </div>
+        </footer>
+      </article>
+    `);
+    return $tweet;
+  };
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    
+    tweets.forEach(element => {
+      const $newTweet = createTweetElement(element)[0];
+      $('#tweets-container').prepend($newTweet);
+    });
+  }
+
+  // $(document).on('dblclick', function (event) {
+  //   console.log(createTweetElement(data[0])[0]);
+  //   // renderTweets(data);
+
+  // });
+  
+  renderTweets(data);
+});
