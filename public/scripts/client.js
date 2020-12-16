cd62cfd2650aa8b771b5bc7ad8fe2fbd2dedb655
justinkwanchan <justinkwanchan@gmail.com>
2020-12-16 14:48:05 +0000

    Created function in client.js to dynamically turn a tweet object into HTML. Also changed avatar image from being a CSS background to an HTML img.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,55 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+$(document).ready(function() {
+  $(document).on('dblclick', function (event) {
+    console.log(createTweetElement(tweetData)[0]); // to see what it looks like
+  });
+});
+
+const createTweetElement = function (tweet) {
+  const $tweet = $(`
+    <article class="hvr-box-shadow-outset">
+      <header>
+        <div>
+          <div>
+            <img src="${tweet.user.avatars}">
+          </div>
+          <span>Newton</span>
+        </div>
+        
+        <span class="handle">${tweet.user.handle}</span>
+      </header>
+
+      <p>
+        ${tweet.content.text}
+      </p>
+
+      <footer>
+        <span class="tweet-age">${tweet.created_at} days ago</span>
+        <div class="icons">
+          <div class="flag"></div>
+          <div class="share"></div>
+          <div class="like"></div>
+        </div>
+      </footer>
+    </article>
+  `);
+  return $tweet;
+};
+
+// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
